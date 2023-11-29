455a9e47ae47a6b1956510a8717c9e05eccb0a1a
Sumeyye <sumeyyedeg00@gmail.com>
2023-11-29 23:20:13 +0300

    Pages and routes have been created.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import About from "./pages/About"
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
 
 export default function App() {
   
   return (
-    <h1 className='text-red-400'>App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/sign-in" element={<SignIn/>}></Route>
+        <Route path="/sign-out" element={<SignUp/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/profile" element={<Profile/>}></Route>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
