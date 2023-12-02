e0d4e9f59d5a6a5b9b5b5ac0627d5639d3d8193a
Sumeyye <sumeyyedeg00@gmail.com>
2023-12-03 00:25:31 +0300

    complete sign in page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -58,6 +58,7 @@ export default function SignUp() {
             <span className='text-blue-700'>Sign in</span>
           </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
