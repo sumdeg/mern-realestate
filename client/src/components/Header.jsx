c4e6a8a454dcec58a70c2a97b1f86bef04740ae4
Sumeyye <sumeyyedeg00@gmail.com>
2023-12-14 22:23:01 +0300

    Add contact landlord functionality to the listing page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,7 +6,7 @@ import {useSelector} from 'react-redux'
 export default function Header() {
     const {currentUser}=useSelector(state=>state.user)
   return (
-    <header className='bg-blue-300 shadow-md'>
+    <header className='bg-gray-500 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
             <Link to="/">
                 <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -5,6 +5,9 @@ import SwiperCore from 'swiper'
 import {Navigation} from 'swiper/modules'
 import 'swiper/css/bundle'
 import {FaBath, FaBed, FaChair, FaMapMarkedAlt, FaParking, FaShare} from 'react-icons/fa'
+import { current } from '@reduxjs/toolkit'
+import { useSelector } from 'react-redux'
+import Contact from '../components/Contact'
 
 export default function Listing() {
     SwiperCore.use([Navigation])
@@ -13,7 +16,8 @@ export default function Listing() {
     const [loading,setLoading]=useState(false)
     const [error, setError] = useState(false)
     const [copied, setCopied] = useState(false)
-
+    const {currentUser}=useSelector(state=>state.user)
+    const [contact, setContact] = useState(false)
     useEffect(() => {
       const fetchListing=async()=>{
 
@@ -95,6 +99,10 @@ export default function Listing() {
                         <FaChair className='text-lg'/> {listing.furnished ? 'Furnished' : 'Unfurnished'}
                       </li>
                    </ul>
+                   {currentUser && listing.userRef !== currentUser._id && !contact && (
+                     <button onClick={()=>setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>Contact Landlord</button>
+                   )}
+                   {contact && <Contact listing={listing}/> }
                 </div>
            </>
         }
